8fd199473f4cc17e87c5d24a3eb41b1ac1dfe6dd
piesieomane <piesieyeboah@gmail.com>
2022-07-12 13:24:37 +0300

    test 3 functions

--- describe.test.js
+++ describe.test.js
@@ -12,3 +12,29 @@ describe("add", () => {
     expect(result).toBe(3);
   });
 });
+
+describe("add", () => {
+  test("2 - 1 = 1", () => {
+    /// arrange
+    const num1 = 2;
+    const num2 = 1;
+    /// act
+    const calculator = new Calculator(num1, num2);
+    const result = calculator.substract(num1, num2);
+    /// assert
+    expect(result).toBe(1);
+  });
+});
+
+describe("divide", () => {
+  test("2/2 = 1", () => {
+    /// arrange
+    const num1 = 2;
+    const num2 = 2;
+    /// act
+    const calculator = new Calculator(num1, num2);
+    const result = calculator.divide(num1, num2);
+    /// assert
+    expect(result).toBe(1);
+  });
+});
